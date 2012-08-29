16e1f69893a4f79cd4525b6d99489f2d2f0c371a
tjuer <zhijianghou@163.com>
2012-08-29 16:43:23 +0800

    add todo list in README add username() function

--- a.c
+++ a.c
@@ -3,6 +3,11 @@
 int main()
 {
     printf("hello world! I'm tjuer!\n");
-    printf("Welcome to github!\n");
+    printf("Welcome to github! %s\n", username());
     return 0;
+}
+
+const char* username()
+{
+    return "supermonkey";
 }
