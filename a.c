575324e7aeaef9acfc1f6a85c605d785ac44e571
tjuer <zhijianghou@163.com>
2012-08-30 17:27:28 +0800

    featureA - 2

--- a.c
+++ a.c
@@ -1,5 +1,8 @@
 #include"a.h"
 
+// pre-declaration
+int add(int x, int y);
+
 int main()
 {
     printf("hello world! I'm tjuer!\n");
@@ -7,10 +10,17 @@ int main()
 
     printf("This is a new feature added on 'featureA' branch!\n");
 
+    printf("result is %d\n", add(1,6));
+
     return 0;
 }
 
 const char* username()
 {
     return "supermonkey";
+}
+
+int add(int x, int y)
+{
+    return x+y;
 }
