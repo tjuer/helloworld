0fb22d81ebe5c9a25c334ef79d556251e938ac5d
tjuer <zhijianghou@163.com>
2012-08-30 17:24:12 +0800

    add new featureA -1

--- a.c
+++ a.c
@@ -4,6 +4,9 @@ int main()
 {
     printf("hello world! I'm tjuer!\n");
     printf("Welcome to github! %s\n", username());
+
+    printf("This is a new feature added on 'featureA' branch!\n");
+
     return 0;
 }
 
